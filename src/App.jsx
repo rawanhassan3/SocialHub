e6fbff083f8acb8cd79b6c0c7a08de93c659d99d
rawanhassan3 <rawan.hassan3@msa.edu.eg>
2026-03-06 00:11:50 +0200

    fix routes and case sensitive imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,81 @@
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
-import Feed from './pages/Feed';
-import Signin from './pages/Signin';
 
-import Signup from './pages/Signup';
-import Profile from './pages/Profile';
-import Postdetails from './pages/Postdetails';
-import Notfound from './pages/Notfound';
-import MainLayout from './Layouts/MainLayout';
-import Authlayout from './Layouts/Authlayout';
-import Protectedroutes from './protectedroutes/protectedroutes';
-import ProtectedAuthroute from './protectedroutes/ProtectedAuthroute';
+import Feed from "./pages/Feed";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import Postdetails from "./pages/Postdetails";
+import Notfound from "./pages/Notfound";
+
+import MainLayout from "./Layouts/MainLayout";
+import Authlayout from "./Layouts/Authlayout";
+
+import ProtectedRoutes from "./protectedroutes/Protectedroutes";
+import ProtectedAuthroute from "./protectedroutes/ProtectedAuthroute";
+
 import AuthContextProvider from "./contexts/Authcontext";
 import CounterContextProvider from "./contexts/Countercontext";
 
-
 const router = createBrowserRouter([
-
   {
-    path: '', element: <MainLayout />, children: [
-      { index: true, element: <Protectedroutes><Feed /></Protectedroutes> },
-      { path: 'Profile', element: <Protectedroutes><Profile /></Protectedroutes> },
-      { path: 'Post/:postId', element: <Protectedroutes><Postdetails /></Protectedroutes> },
-      { path: '*', element: <Notfound /> },
-      { path: 'postdetails', element: <Postdetails /> }
-    ]
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: (
+          <ProtectedRoutes>
+            <Feed />
+          </ProtectedRoutes>
+        ),
+      },
+      {
+        path: "profile",
+        element: (
+          <ProtectedRoutes>
+            <Profile />
+          </ProtectedRoutes>
+        ),
+      },
+      {
+        path: "post/:postId",
+        element: (
+          <ProtectedRoutes>
+            <Postdetails />
+          </ProtectedRoutes>
+        ),
+      },
+      { path: "*", element: <Notfound /> },
+    ],
   },
   {
-    path: '', element: <Authlayout />, children: [
-      { path: 'Signin', element: <ProtectedAuthroute><Signin /></ProtectedAuthroute> },
-      { path: 'Signup', element: <ProtectedAuthroute><Signup /></ProtectedAuthroute> },
-      ]
-  }
-])
+    path: "/",
+    element: <Authlayout />,
+    children: [
+      {
+        path: "signin",
+        element: (
+          <ProtectedAuthroute>
+            <Signin />
+          </ProtectedAuthroute>
+        ),
+      },
+      {
+        path: "signup",
+        element: (
+          <ProtectedAuthroute>
+            <Signup />
+          </ProtectedAuthroute>
+        ),
+      },
+    ],
+  },
+]);
+
 export default function App() {
-  return <>
+  return (
     <AuthContextProvider>
       <CounterContextProvider>
         <HeroUIProvider>
@@ -45,8 +84,5 @@ export default function App() {
         </HeroUIProvider>
       </CounterContextProvider>
     </AuthContextProvider>
-
-
-
-  </>
+  );
 }
